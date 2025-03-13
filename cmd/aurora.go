57b8d135acc4c2eeb9a1bdf9db7801961aa09e80
Firdavs <firdavsreadmi8@gmail.com>
2025-03-13 15:34:41 +0300

    feat: expand shell command list for enhanced user interaction
    - Updated the list of predefined shell commands in `cmd/aurora.go` to include a comprehensive set of commands categorized by functionality.
    - Improved command recognition and user experience by organizing commands into sections such as user information, file manipulation, system monitoring, and more.

--- cmd/aurora.go
+++ cmd/aurora.go
@@ -16,14 +16,55 @@ func init() {
 }
 
 var shellCommands = []string{
-	"ls", "cd", "pwd", "echo", "rm",
-	"mkdir", "rmdir", "mv", "cp", "touch",
-	"cat", "head", "tail", "grep", "find",
-	"chmod", "chown", "tar", "zip", "unzip",
-	"git", "docker", "kubectl", "python", "go",
-	"htop", "ping", "kill", "jobs", "fg", "bg",
-	"whoami", "uname", "df", "du", "ps", "top",
+	// User information and system data
+	"whoami", "id", "uname", "hostname", "uptime", "w", "who", "groups",
+	"whois", "finger", "last", "lastlog", "lastb", "lastcomm", "lastcomm",
 	"clear", "help", "exit", "quit", "cls", "clr",
+
+	// User and permissions
+	"su", "sudo", "passwd", "chown", "chmod", "chgrp", "umask",
+
+	// Folders and files
+	"ls", "ll", "la", "pwd", "cd", "mkdir", "rmdir", "touch", "rm", "mv", "cp",
+	"find", "locate", "updatedb", "tree",
+
+	// File and text manipulation
+	"cat", "tac", "less", "more", "head", "tail", "grep", "awk", "sed", "cut",
+	"sort", "uniq", "tee", "wc", "diff", "cmp",
+
+	// Processes and system monitoring
+	"ps", "top", "htop", "nice", "renice", "kill", "pkill", "jobs", "fg", "bg",
+	"nohup", "time", "strace", "lsof",
+
+	// Disk and file system
+	"df", "du", "mount", "umount", "fsck", "mkfs", "blkid", "fdisk", "parted",
+	"lsblk", "e2fsck", "sync", "tune2fs",
+
+	// Network and internet
+	"ping", "traceroute", "netstat", "ss", "ifconfig", "ip", "iwconfig",
+	"curl", "wget", "scp", "rsync", "nc", "telnet", "ftp", "sftp",
+
+	// Archiving and compression
+	"tar", "zip", "unzip", "gzip", "gunzip", "bzip2", "bunzip2", "xz", "7z",
+	"rar", "unrar",
+
+	// Software and package management
+	"apt", "apt-get", "yum", "dnf", "pacman", "zypper", "brew", "snap", "flatpak",
+	"dpkg", "rpm", "pip", "gem", "npm", "cargo", "go", "python", "ruby", "perl",
+
+	// Docker and containers
+	"docker", "docker-compose", "podman", "kubectl", "minikube",
+
+	// Git and version control
+	"git", "git clone", "git pull", "git push", "git commit", "git status",
+	"git log", "git branch", "git checkout", "git merge", "git rebase",
+
+	// Programming languages and compilers
+	"python", "python3", "node", "nodejs", "npm", "npx", "java", "javac",
+	"ruby", "perl", "php", "php-cli", "go", "rustc", "gcc", "g++",
+
+	// Shell and scripts
+	"bash", "sh", "zsh", "fish", "dash", "tcsh", "csh", "ksh",
 }
 
 // ProcessAuroraCommand handles Aurora-specific commands
